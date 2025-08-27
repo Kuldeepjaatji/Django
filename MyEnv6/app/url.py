6af20e078da59bd905bc8663718550bdc05667bb
Kuldeep_jaat_ji <ks7799952@gmail.com>
2025-08-27 16:25:44 +0530

    'first_comment'

--- MyEnv6/app/url.py
+++ MyEnv6/app/url.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('marksheet/', views.student_marksheet, name='marksheet'),
     path('login',views.login_view , name='login'),
     path('register',views.register,name='register'),
-    path('thank_you',views.thank_you,name='thank_you')
+    path('thank_you',views.thank_you,name='thank_you'),
+    path('student_list',views.student_list,name='student_list'),
+    path('studentAdd',views.studentAdd,name='studentAdd'),
+    path('update_student/<int:pk>',views.update_student,name='update_student'),
+    path('delete_student/<int:pk>',views.delete_student,name='delete_student')
 ]
--- MyEnv6/app/views.py
+++ MyEnv6/app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render , redirect
-from .models import student, login
+from django.shortcuts import render , redirect , get_object_or_404
+from .models import student, login , StudentCorse
 from django.db import IntegrityError
 from .forms import StudentForm
 
@@ -97,3 +97,34 @@ def register(req):
     else:
         form = StudentForm()
     return render(req,'register.html',{'form' : form})
+
+def studentAdd(req):
+    if req.method == 'POST':
+        form = StudentForm(req.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(req,'studentAdd.html',{'form' : form})
+
+
+
+def student_list(req):
+    students = StudentCorse.objects.all()
+    return render(req,'student_list.html',{'students':students})
+
+
+
+def update_student(req,pk):
+    student = get_object_or_404(StudentCorse,pk=pk)
+    form = StudentForm(req.POST or None,instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(req,'studentAdd.html',{'form':form})
+def delete_student(req,pk):
+    student = get_object_or_404(StudentCorse,pk=pk)
+    student.delete()
+    students = StudentCorse.objects.all()
+    return render(req, 'student_list.html', {'students': students})
